dfac42b7269db90a3ec22944d822b24113424407
Tarshish0906 <k.oxygen08@gmail.com>
2021-02-03 05:30:57 +0900

    -nオプションの追加

--- mygrep/src/main.rs
+++ mygrep/src/main.rs
@@ -7,24 +7,32 @@ struct MyGrep {
     pattern: String,
     #[structopt(name = "FILE")]
     path: String,
+    #[structopt(short = "-n", long)]
+    line_number: bool,
 }
 
-fn grep(content: String, pattern: String) {
+fn grep(mygrep: MyGrep, content: String) {
+    let mut line_number = 1;
     for line in content.lines() {
-        if line.contains(pattern.as_str()) {
-            println!("{}", line);
+        if line.contains(mygrep.pattern.as_str()) {
+            if mygrep.line_number {
+                println!("{}: {}", line_number, line);
+            } else {
+                println!("{}", line);
+            }
         }
+        line_number += 1;
     }
 }
 
 fn run(mygrep: MyGrep) {
-    match std::fs::read_to_string(mygrep.path) {
-        Ok(content) => grep(content, mygrep.pattern),
+    match std::fs::read_to_string(&mygrep.path) {
+        Ok(content) => grep(mygrep, content),
         Err(reason) => println!("{}", reason),
     }
 }
 
 fn main() {
-    let mygrep = MyGrep::from_arg();
+    let mygrep = MyGrep::from_args();
     run(mygrep);
 }
